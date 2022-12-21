08f2bad786f507c496d3bac1169a68413ff676e0
clyde0813 <eric3285@naver.com>
2022-12-21 17:01:57 +0900

    Initial Commit

---
+++ Chatty_Swift/Chatty_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chatty_SwiftApp.swift
+//  Chatty_Swift
+//
+//  Created by Clyde on 2022/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Chatty_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
