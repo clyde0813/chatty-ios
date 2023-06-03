0248883536881a9ac60512a7b4a6d2b3dd722e92
hyeonho <gusgh4922@gmail.com>
2023-06-04 01:52:49 +0900

    폴더 및 파일 생성

---
+++ Chatty/View/Timeline/TimelineView.swift
@@ -0,0 +1,20 @@
+//
+//  TimelineView.swift
+//  Chatty
+//
+//  Created by Hyeonho on 2023/06/04.
+//
+
+import SwiftUI
+
+struct TimelineView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TimelineView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimelineView()
+    }
+}
