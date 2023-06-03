38b12cae55e2c318c43c3fc5a556cdf508c0e4e2
hyeonho <gusgh4922@gmail.com>
2023-06-04 03:35:14 +0900

    tab바 추가

--- Chatty/View/Timeline/TimelineView.swift
+++ Chatty/View/Timeline/TimelineView.swift
@@ -1,14 +1,28 @@
 import SwiftUI
 
+enum Timeline_Hot_Tab {
+    case timeline,hotQuestion
+}
+
 struct TimelineView: View {
+    
+    @State var currentTab : Timeline_Hot_Tab =  .timeline
+    
     var body: some View {
         VStack{
-            navBar
-            HStack{
-                
+            VStack{
+                navBar
+                tabChangeBar
             }
+            .background(Rectangle()
+                .fill(Color.white)
+                .shadow(color: Color("Shadow Button"), radius: 3, x: 0, y: 6)
+            )
+            
             Spacer()
         }
+        
+        
         
     }
 }
@@ -31,12 +45,63 @@ extension TimelineView {
         }
         .padding(.horizontal, 16)
         .frame(height: 60)
-        .background(Rectangle()
-            .fill(Color.white)
-            .shadow(color: Color("Shadow Button"), radius: 3, x: 0, y: 6)
-        )
+    }
+    var tabChangeBar : some View {
+        HStack(spacing: 20){
+            Spacer()
+            ZStack(alignment: .bottom){
+                Button(action: {
+                    currentTab = .timeline
+                }){
+                    if currentTab == .timeline {
+                        VStack(alignment: .center, spacing: 0){
+                            Text("타임라인")
+                                .font(Font.system(size: 16, weight: .bold))
+                                .accentColor(.black)
+                                .padding(.bottom, 9)
+                            Rectangle()
+                                .fill(Color("Main Secondary"))
+                                .frame(width: 50, height: 3)
+                        }
+                    } else {
+                        Text("타임라인")
+                            .font(Font.system(size: 16, weight: .semibold))
+                            .foregroundColor(Color.gray)
+                            .padding(.bottom, 12)
+                    }
+                }
+                .accentColor(.black)
+            }
+            Spacer()
+            ZStack(alignment: .bottom){
+                Button(action: {
+                    currentTab = .hotQuestion
+                }){
+                    if currentTab == .hotQuestion {
+                        VStack(alignment: .center, spacing: 0){
+                            Text("지금 핫한 질문")
+                                .font(Font.system(size: 16, weight: .bold))
+                                .accentColor(.black)
+                                .padding(.bottom, 9)
+                            Rectangle()
+                                .fill(Color("Main Secondary"))
+                                .frame(width: 50, height: 3)
+                        }
+                    } else {
+                        Text("지금 핫한 질문")
+                            .font(Font.system(size: 16, weight: .semibold))
+                            .foregroundColor(Color.gray)
+                            .padding(.bottom, 12)
+                    }
+                }
+                .accentColor(.black)
+            }
+            Spacer()
+        }
+        .padding(.top,10)
     }
 }
+
 
 struct TimelineView_Previews: PreviewProvider {
     static var previews: some View {
