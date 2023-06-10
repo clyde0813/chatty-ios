aec891fd8acb00d3a5ec59811268699fbcbe48f3
hyeonho <gusgh4922@gmail.com>
2023-06-11 04:06:30 +0900

    FollowView add Tabbar

--- Chatty/View/Follow/FollowView.swift
+++ Chatty/View/Follow/FollowView.swift
@@ -1,18 +1,24 @@
-//
-//  FollowView.swift
-//  Chatty
-//
-//  Created by Hyeonho on 2023/06/11.
-//
-
 import SwiftUI
-
+enum followTab {
+    case follow,following
+}
 struct FollowView: View {
     @Environment(\.dismiss) var dismiss
+    
     @Binding var username : String
+    
+    @State var currentTab : followTab = .follow
+    
     var body: some View {
         VStack{
-            navView
+            VStack{
+                navView
+                tabChangeBar
+            }
+            .background(Rectangle()
+                .fill(Color.white)
+                .shadow(color: Color("Shadow Button"), radius: 3, x: 0, y: 4)
+            )
             
             Spacer()
         }
@@ -40,6 +46,70 @@ extension FollowView {
         }
         .padding(.horizontal,24)
         .padding(.vertical,14)
+    }
+    
+    var tabChangeBar : some View {
+        HStack(spacing: 20){
+            Spacer()
+            ZStack(alignment: .bottom){
+                Button(action: {
+                    currentTab = .follow
+                }){
+                    if currentTab == .follow {
+                        VStack(alignment: .center, spacing: 0){
+                            Text("팔로우")
+                                .font(Font.system(size: 16, weight: .bold))
+                                .accentColor(.black)
+                                .padding(.bottom, 9)
+                            Rectangle()
+                                .fill(Color("Main Secondary"))
+                                .frame(width: 50, height: 3)
+                        }
+                    } else {
+                        VStack{
+                            Text("팔로우")
+                                .font(Font.system(size: 16, weight: .semibold))
+                                .foregroundColor(Color.gray)
+                            Rectangle()
+                                .fill(Color("Main Secondary"))
+                                .frame(width: 50, height: 0)
+                        }
+                    }
+                }
+                .accentColor(.black)
+            }
+            Spacer()
+            ZStack(alignment: .bottom){
+                Button(action: {
+                    currentTab = .following
+                }){
+                    if currentTab == .following {
+                        VStack(alignment: .center, spacing: 0){
+                            Text("팔로잉")
+                                .font(Font.system(size: 16, weight: .bold))
+                                .accentColor(.black)
+                                .padding(.bottom, 9)
+                            Rectangle()
+                                .fill(Color("Main Secondary"))
+                                .frame(width: 50, height: 3)
+                        }
+                    } else {
+                        VStack{
+                            Text("팔로잉")
+                                .font(Font.system(size: 16, weight: .semibold))
+                                .foregroundColor(Color.gray)
+                            Rectangle()
+                                .fill(Color("Main Secondary"))
+                                .frame(width: 50, height: 0)
+                        }
+                        
+                    }
+                }
+                .accentColor(.black)
+            }
+            Spacer()
+        }
+        .padding(.top,10)
     }
 }
 
