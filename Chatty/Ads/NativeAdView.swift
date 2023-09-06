d46dcbc872f89d4832a690a4f87a891da30d1033
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2023-09-07 04:23:26 +0900

    not Fix Ranking

---
+++ Chatty/Ads/NativeAdView.swift
@@ -0,0 +1,8 @@
+//
+//  NativeAdView.swift
+//  Chatty
+//
+//  Created by Hyeonho on 2023/09/02.
+//
+
+import Foundation
---
+++ Chatty/Ads/TestAdsView.swift
@@ -0,0 +1,20 @@
+//
+//  TestAdsView.swift
+//  Chatty
+//
+//  Created by Hyeonho on 2023/09/02.
+//
+
+import SwiftUI
+
+struct TestAdsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TestAdsView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestAdsView()
+    }
+}
